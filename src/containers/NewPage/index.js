42e807719eb3c4c4ea6e5faf4d0e1b01cc0a012e
Hoan Vu Van <congtunggv@gmail.com>
2021-03-01 15:20:08 +0700

    create action redux add page

--- src/containers/NewPage/index.js
+++ src/containers/NewPage/index.js
@@ -4,7 +4,8 @@ import Input from '../../components/UI/Input';
 import Modal from '../../components/UI/Modal';
 import { Row, Col, Container } from 'react-bootstrap';
 import linearCategories from '../../helpers/linearCategories';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { createPage } from '../../redux/actions'
 
 const NewPage = () => {
     const [createModal, setCreateModal] = useState(false);
@@ -13,20 +14,52 @@ const NewPage = () => {
     const [categories, setCategories] = useState([]);
     const [categoryId, setCategoryId] = useState('');
     const [desc, setDesc] = useState('');
+    const [type, setType] = useState('');
     const [banners, setBanners] = useState([]);
     const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         setCategories(linearCategories(category.categories));
-        ;
     }, [category]);
 
+    const onCategoryChange = (e) => {
+        categories.find(category => category._id == e.target.value);
+        setCategoryId(e.target.value);
+        setType(category.type);
+    }
     const handleBannerImages = (e) => {
         console.log(e);
+        setBanners([...banners, e.target.files[0]]);
     }
 
     const handleProductImages = (e) => {
         console.log(e);
+        setProducts([...products, e.target.files[0]]);
+    }
+
+    const submitPageForm = (e) => {
+        // e.target.preventDefault();
+
+        if(title === ""){
+            alert('Title is required');
+            setCreateModal(false);
+            return;
+        }
+        const form = new FormData();
+        form.append('title', title);
+        form.append('description', desc);
+        form.append('category', categoryId);
+        form.append('type', type);
+        banners.forEach((banner, index) => {
+            form.append('banners', banner);
+        });
+        products.forEach((product, index) => {
+            form.append('products', product);
+        });
+
+        dispatch(createPage(form));
+
     }
 
     const renderCreatePageModal = () => {
@@ -35,6 +68,7 @@ const NewPage = () => {
                 show={createModal}
                 modalTitle={'Create new page'}
                 handleClose={() => setCreateModal(false)}
+                handleSave={submitPageForm}
             >
                 <Container>
                     <Row>
@@ -42,7 +76,7 @@ const NewPage = () => {
                             <select
                             className="form-control form-control-sm" 
                                 value={categoryId}
-                                onChange={(e) => setCategoryId(e.target.value)}
+                                onChange={onCategoryChange}
                             >
                                 <option value="">select category</option>
                                 {
@@ -74,7 +108,14 @@ const NewPage = () => {
                             />
                         </Col>
                     </Row>
-
+                    {
+                        banners.length > 0 ?
+                        banners.map((banner, index) =>
+                            <Row key={index}>
+                                <Col>{banner.name}</Col>
+                            </Row>   
+                        ) : null
+                    }
                     <Row>
                         <Col>
                             <Input
@@ -85,7 +126,14 @@ const NewPage = () => {
                             />
                         </Col>
                     </Row>
-
+                    {
+                        products.length > 0 ?
+                        products.map((product, index) =>
+                            <Row key={index}>
+                                <Col>{product.name}</Col>
+                            </Row>   
+                        ) : null
+                    }
                     <Row>
                         <Col>
                             <Input
