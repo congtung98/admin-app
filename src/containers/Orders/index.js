80821bcbc104baef4aa244c77ed9289eaf10b3dd
Hoan Vu Van <congtunggv@gmail.com>
2021-03-24 10:56:15 +0700

    update order

--- src/containers/Orders/index.js
+++ src/containers/Orders/index.js
@@ -1,10 +1,54 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Layout from '../../components/Layout'
+import Card from '../../components/UI/Card';
 
-const Orders = () => {
+import "./style.css";
+
+const Orders = (props) => {
+    const order = useSelector((state) => state.order);
+
     return (
         <Layout sidebar>
-            Orders
+            <Card headerLeft="ORDER_ID">
+                <div
+                    style={{
+                        boxSizing: "border-box",
+                        padding: "100px"
+                    }}
+                >
+                    <div className="orderTrack">
+                        <div className="orderStatus">
+                            <div className="point"></div>
+                            <div className="orderInfo">
+                                <div className="status">Ordered</div>
+                                <div className="date">Fri, 2021</div>
+                            </div>
+                        </div>
+                        <div className="orderStatus">
+                            <div className="point"></div>
+                            <div className="orderInfo">
+                                <div className="status">Packed</div>
+                                <div className="date">Fri, 2021</div>
+                            </div>
+                        </div>
+                        <div className="orderStatus">
+                            <div className="point"></div>
+                            <div className="orderInfo">
+                                <div className="status">Shipped</div>
+                                <div className="date">Fri, 2021</div>
+                            </div>
+                        </div>
+                        <div className="orderStatus">
+                            <div className="point"></div>
+                            <div className="orderInfo">
+                                <div className="status">Delivered</div>
+                                <div className="date">Fri, 2021</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Card>
         </Layout>
     )
 }
